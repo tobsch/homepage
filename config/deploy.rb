5800fb6cb6feac6f440885efb8cabe9b6f9479ff
Tobias Schlottke <tobias.schlottke@gmail.com>
2011-05-28 10:39:08 +0200

    yay

--- config/deploy.rb
+++
@@ -1,9 +0,0 @@
-set :application, 'tobias-schlottke'
-
-SKIP_DEFAULT_ROLES = true
-role :app, 's3.adfarm.adyard.de'
-role :web, 's3.adfarm.adyard.de'
-role :db,  's3.adfarm.adyard.de', :primary => true
-
-load_paths << 'vendor/plugins/common_deploy/lib'
-load 'cd_standard'
