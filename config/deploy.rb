5ee4a898e4757b7d5342b9d7e4719575a1300b03
Tobias Schlottke <tobias.schlottke@gmail.com>
2014-06-08 16:25:03 +0200

    updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,10 @@ require 'common_deploy'
 
 set :git_server, 'git@git.adyard.de'
 set :application, 'tobias-schlottke'
-set :repository, 'git@git.adyard.de:tobias-schlottke.git'
-set :tld, 'de'
+set :repository, 'git@git.metrigo.com:root/homepage.git'
 
 load_extensions :rvm, :bundler, :passenger, :assets
 
-role :app, 's1.hamburg-ventures.de'
-role :web, 's1.hamburg-ventures.de'
-role :db,  's1.hamburg-ventures.de', :primary => true
+role :app, 's2.hamburg-ventures.de'
+role :web, 's2.hamburg-ventures.de'
+role :db,  's2.hamburg-ventures.de', :primary => true
